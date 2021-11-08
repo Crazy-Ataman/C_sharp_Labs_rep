867d2616a3cb822e1e17b6e5ca36997813a81916
Crazy-Ataman <maksdadapayr@mail.ru>
2021-11-08 13:25:07 +0300

    Completed Lab9_sharp

---
+++ Lab9_sharp/Lab9_sharp/Director.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lab9_sharp
+{
+    class Director
+    {
+        public delegate void DirectorHandler();
+        public event DirectorHandler Raise;
+        public event DirectorHandler Fine;
+
+        public string Name { get; set; }
+        public int Money { get; set; }
+        public int FineCounter { get; set; } = 0;
+
+        public Director(string name, int money)
+        {
+            Name = name;
+            Money = money;
+        }
+
+        public void ShowInfo() 
+            => Console.WriteLine($"{Name} has money {Money}$. The number of fines: {FineCounter}.");
+
+        public void RaiseDirector()
+        {
+            Money += 500;
+            Raise?.Invoke();
+        }
+
+        public void GetFine()
+        {
+            Money -= 300;
+            FineCounter++;
+            Fine?.Invoke();
+        }
+    }
+}
---
+++ Lab9_sharp/Lab9_sharp/Students.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab9_sharp
+{
+    public class Students
+    {
+        private string Name { get; set; }
+
+        public Students(string name) => Name = name;
+
+        public void GossipAboutRaise()
+            => Console.WriteLine($"[{Name}] You heard?! Our director got a raise. About five hundred bucks!");
+
+        public void GossipAboutFine()
+            => Console.WriteLine($"[{Name}] You heard?! Our director got a fine. About three hundred bucks!");
+
+    }
+}

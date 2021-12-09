a03514c1795232d19730eb395c5d71f6039a6b2a
Crazy-Ataman <maksdadapayr@mail.ru>
2021-12-09 23:53:33 +0300

    Completed Lab15_sharp

---
+++ Lab15_sharp/Lab15_sharp/OddAndEven.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace Lab15_sharp
+{
+    internal static class OddAndEven
+    {
+
+
+        internal static void SingleThread(int n)
+        {
+            FileInfo file = new FileInfo(@"..\..\..\First_even,then_odd.txt");
+            if (file.Exists)
+            {
+                file.Delete();
+            }
+            object locker = new object();
+            bool acquiredLock = false;
+            Thread OddThread = new Thread(new ThreadStart(PrintOdd));
+            Thread EvenThread = new Thread(new ThreadStart(PrintEven));
+            EvenThread.Start();
+            OddThread.Start();
+
+            void PrintOdd()
+            {
+                Monitor.Enter(locker, ref acquiredLock);
+                var odds = new List<int>();
+                Thread.CurrentThread.Priority = ThreadPriority.BelowNormal;
+                for (int i = 1; i <= n; i += 2)
+                {
+                    Thread.Sleep(20);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    //Console.Write(i + " ");
+                    Console.Write($"{i} ");
+                    odds.Add(i);
+                }
+                using var writer = new StreamWriter(@"..\..\..\First_even,then_odd.txt", true);
+                writer.Write(string.Join(" ", odds));
+                writer.Close();
+                Monitor.Exit(locker);
+            }
+
+            void PrintEven()
+            {
+                lock (locker)
+                {
+                    var evens = new List<int>();
+                    for (int i = 0; i <= n; i += 2)
+                    {
+                        Thread.Sleep(150);
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write($"{i} ");
+                        evens.Add(i);
+                    }
+                    using var writer = new StreamWriter(@"..\..\..\First_even,then_odd.txt", true);
+                    writer.Write(string.Join(" ", evens));
+                    writer.Write(" ");
+                    writer.Close();
+                }
+            }
+        }
+
+        public static void Together(int n)
+        {
+            using var writer = new StreamWriter(@"..\..\..\Odd,even,odd,even.txt");
+            Mutex mutex = new Mutex();
+            Thread OddThread = new Thread(new ThreadStart(PrintOdd));
+            Thread EvenThread = new Thread(new ThreadStart(PrintEven));
+            EvenThread.Start();
+            OddThread.Start();
+            EvenThread.Join();
+            OddThread.Join();
+
+            void PrintOdd()
+            {
+                for (int i = 1; i <= n; i += 2)
+                {
+                    // mutexObj.WaitOne() suspends thread execution until the mutexObj mutex is acquired.
+                    mutex.WaitOne();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"{i} ");
+                    writer.Write($"{i} ");
+                    // The thread releases it using the mutexObj.ReleaseMutex () method.
+                    mutex.ReleaseMutex();
+                }
+            }
+
+            void PrintEven()
+            {
+                for (int i = 0; i <= n; i += 2)
+                {
+                    Thread.CurrentThread.Priority = ThreadPriority.Lowest;
+                    mutex.WaitOne();
+                    Thread.Sleep(50);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write($"{i} ");
+                    writer.Write($"{i} ");
+                    mutex.ReleaseMutex();
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Lab15_sharp/Lab15_sharp/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lab15_sharp
+{
+    internal class Program
+    {
+        public static bool IsPrime(int number)
+        {
+            switch (number)
+            {
+                case <= 1:
+                    return false;
+                case 2:
+                    return true;
+            }
+
+            if (number % 2 == 0)
+                return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (var i = 3; i <= boundary; i += 2)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            var allProcess = Process.GetProcesses();
+            Console.WriteLine($"{"ID", -11} {"Name", -34} {"Priority", -40}");
+            Console.WriteLine(new string('_', 55));
+            foreach (var proc in allProcess)
+            {
+                Console.WriteLine("{0, -9} {1, -36} {2, -70}",
+                    "|" + $"{proc.Id}".PadRight(9) + "|", 
+                    $"{proc.ProcessName}".PadRight(36) + "|", 
+                    $"{proc.BasePriority}".PadRight(2) + "|");
+
+                // proc.StartTime - protected by some processes. Usually svhost, windows and etc.
+                // try and catch need to prevent unhandle exception
+                //try
+                //{
+                //    Console.WriteLine($"ID: {proc.Id}\tStart time: {proc.StartTime}\tName: {proc.ProcessName} \tPriority: {proc.BasePriority}");
+                //}
+
+                //catch
+                //{
+                //    Console.WriteLine($"Access denied {proc.ProcessName}");
+                //}
+            }
+            Console.WriteLine(new string('-', 55));
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            AppDomain originalDomain = AppDomain.CurrentDomain;
+            // Friendly in this context is understandably.
+            Console.WriteLine($"Name: {originalDomain.FriendlyName}");
+            Console.WriteLine($"Details: {originalDomain.SetupInformation}");
+            Console.WriteLine($"Base directory: {originalDomain.BaseDirectory}");
+            Console.WriteLine($"Assemblies: ");
+            originalDomain.GetAssemblies().ToList().ForEach(x => Console.WriteLine($"\t{x.FullName}"));
+
+            // .NET FrameWork, not .NET Core
+            AppDomain newDomain = AppDomain.CreateDomain("New Domain");
+            newDomain.Load(Assembly.GetExecutingAssembly().FullName);
+
+            Console.WriteLine($"\nName: {newDomain.FriendlyName}");
+            Console.WriteLine($"Details: {newDomain.SetupInformation}");
+            Console.WriteLine($"Base directory: {newDomain.BaseDirectory}");
+            Console.WriteLine($"Assemblies: ");
+            newDomain.GetAssemblies().ToList().ForEach(x => Console.WriteLine($"\t{x.FullName}"));
+
+            AppDomain.Unload(newDomain);
+
+            // Exception - newDomain unloaded, otherwise - something go wrong.
+            //Console.WriteLine($"\nName: {newDomain.FriendlyName}");
+
+
+            Console.Write("\nInput number for count primes number: ");
+            var n = int.Parse(Console.ReadLine());
+            Thread numbersThread = new Thread(()=>
+            {
+                Thread.CurrentThread.Name = "myThread";
+                Thread.CurrentThread.Priority = ThreadPriority.Highest;
+                Thread.Sleep(3000);
+                var primes = new List<int>();
+                for (var i = 1; i <= n; i++)
+                {
+                    if (IsPrime(i))
+                    {
+                        primes.Add(i);
+                    }
+                }
+                Console.WriteLine(new string('-', 55));
+                Console.WriteLine($"Status: {Thread.CurrentThread.ThreadState}");
+                Console.WriteLine($"Name: {Thread.CurrentThread.Name}");
+                Console.WriteLine($"Priority: {Thread.CurrentThread.Priority}");
+                Console.WriteLine($"ID: {Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine(new string('-', 55));
+                using var writer = new StreamWriter(@"..\..\..\primes.txt");
+                writer.WriteLine(string.Join(", ", primes));
+                Console.WriteLine(string.Join(", ", primes));
+            });
+
+           
+            numbersThread.Start();
+            Thread.Sleep(1000);
+            Console.WriteLine(new string('-', 55));
+            Console.WriteLine($"Status: {numbersThread.ThreadState}");
+            Console.WriteLine($"Name: {numbersThread.Name}");
+            Console.WriteLine($"Priority: {numbersThread.Priority}");
+            Console.WriteLine($"ID: {numbersThread.ManagedThreadId}");
+            Console.WriteLine(new string('-', 55));
+
+            Console.Write("\n");
+
+            Thread.Sleep(2300);
+            Console.WriteLine(new string('-', 55));
+            Console.WriteLine($"Status: {numbersThread.ThreadState}");
+            Console.WriteLine($"Name: {numbersThread.Name}");
+            // You can't to get priority from aborted thread.
+            //Console.WriteLine($"Priority: {numbersThread.Priority}");
+            Console.WriteLine($"ID: {numbersThread.ManagedThreadId}");
+            Console.WriteLine(new string('-', 55));
+
+            Console.Write("Input number: ");
+            n = int.Parse(Console.ReadLine());
+            OddAndEven.SingleThread(n);
+            Console.WriteLine("At first even numbers, after that odd numbers.");
+
+            Thread.Sleep(5000);
+            Console.WriteLine("\nEven, odd, even, odd...");
+            OddAndEven.Together(n);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\n");
+            TimerCallback timerCallback = new TimerCallback(CurrentTime);
+            Timer timer = new Timer(timerCallback, null, 0, 1000);
+            Thread.Sleep(3000);
+            void CurrentTime(object obj)
+            {
+                Console.WriteLine(DateTime.Now);
+            }
+        }
+    }
+}
